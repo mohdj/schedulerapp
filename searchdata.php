b78e1c010334a849dabbdd47184d449f2a919c5e
Ismail S <ismail.sidhpuri@iris.co.uk>
2016-10-12 14:16:25 +0530

    search data add option

--- searchdata.php
+++ searchdata.php
@@ -1,5 +1,6 @@
 <?php
 include('connection.php');
+error_reporting(0);
 
 if (mysqli_connect_errno())
   {
@@ -8,7 +9,6 @@ if (mysqli_connect_errno())
 if($_GET)
 {
     $query="SELECT * FROM daily_hifz_report WHERE its_student = '".addslashes($_GET['itsid'])."' limit 5";
-    $query1="SELECT * FROM students WHERE ITS = '".addslashes($_GET['itsid'])."'";
     $result = mysqli_query($link,$query);
 
 
@@ -16,7 +16,40 @@ if($_GET)
 ?>
 <!DOCTYPE html>
 <html>
+<head>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+ 
+</head>
 <body>
+
+  <div class="modal" id="myModal">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title">Enter hifz data</h4>
+      </div>
+      <div class="modal-body">
+        <div id="hisabform">
+        <select name="salary">
+                            <option value='Cash'>Murajeat</option>
+                            <option value='Zabihat'>Jadeed</option>
+                            <option value='BB Salary'>Juzhali</option>
+                            <option value='SF Salary'>Tasmee</option>
+        </select>  
+        <input type="number" name="Amount" placeholder="Marks"/>
+        <input type="hidden" name="Month"/>
+        <input type="text" class="gregdate" name="sf_amount_date" value="<?php echo date("Y-m-d") ?>"/>
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal" name="cancel">Close</button>
+        <button type="button" class="btn btn-primary" name="save">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 <form>
   <fieldset>
     <legend>Search Data</legend>
@@ -42,6 +75,7 @@ if($_GET)
                     <th>Tasmee Marks</th>
                     <th>Juzhali</th>
                     <th>Juzhali Marks</th>
+                    <th>Action</th>
                   </tr>
                 </thead>
 
@@ -62,8 +96,60 @@ if($_GET)
                     <td><?php echo $values['juzhali']; ?></td>
                   </tr>
                   <?php } ?>
+                  <tr>
+                      <td colspan='9'></td>
+                      <td><a href="#" data-key="payhisab"><img src="images/add.png" style="width:20px;height:20px;"></a></td>
+                  </tr>
                 </tbody>
               </table>
 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script>
+$(function(){
+      $(function(){
+      var hisabform = $('#myModal');
+      hisabform.hide();
+      $('[data-key="payhisab"]').click(function() {
+        $('[name="Month"]', hisabform).val($(this).attr('data-month'));
+        hisabform.show();
+      });
+      $('[name="save"]').click(function() {
+        var data = '';
+        $('input[type!="button"],select', hisabform).each(function() {
+          data = data + $(this).attr('name') + '=' + $(this).val() + '&';
+        });
+        $.ajax({
+          method: 'post',
+          url: '_payhisab.php',
+          async: 'false',
+          data: data,
+          success: function(data) {
+            if(data == 'success') {
+              hisabform.hide();
+              window.location.href = window.location.href; //reload
+            // } else if(data == 'DuplicateReceiptNo') {
+            //   alert('Receipt number already exists in database');
+            }
+            else {
+              alert('Update failed. Please do not add receipt again unless you check system values properly');
+            }
+          },
+          error: function() {
+            alert('Try again');
+          }
+        });
+      });
+
+      $('[name="cancel"]').click(function() {
+        hisabform.hide();
+      });
+
+
+      
+      });
+    });
+  </script>
 </body>
 </html>
--- studentregistration.php
+++ studentregistration.php
@@ -51,47 +51,120 @@ if($_POST)
 
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Hifz Program</title>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <style>
+      .header {
+        min-height: 50px;
+        background: #FFEAC3;
+        text-transform: uppercase;
+        text-align: center;
+        border-bottom: 2px solid #704d0e;
+        margin-bottom: 20px
+      }
+      .header h3 {
+        margin: auto;
+        line-height: 60px;
+      }
+    </style>
+  </head>
 <body>
 
-<form method="post">
-  <fieldset>
-    <legend>Muntasebaat Hifz Registration</legend>
-    Full name:<br>
-    <input type="text" name="fullname" value="">
-    <br>
-    Jamea Farig Year :<br>
-    <input type="text" name="farigyear" value="">
-    </br>
-	Hifz Done Till:<br>
-    <input type="text" name="hifzdonetill" value="">
-    <br>
-	Place of Residence:<br>
-    <input type="text" name="residence" value="">
-    <br>
-	Khidmat, if any:<br>
-    <input type="text" name="khidmat" value="">
-    <br>
-	Email Id:<br>
-    <input type="text" name="email" value="">
-    <br>
-	Skype ID :<br>
-    <input type="text" name="skype" value="">
-    <br>
-	Mobile No:<br>
-    <input type="text" name="mobile" value="">
-    <br>
-	Hifz Class Time:<br>
-    <input type="text" name="hifztime" value="">
-    <br>
-	Hifz Days:<br>
-    <input type="text" name="hifzdays" value="">
-    <br>
-	ITS ID *:<br>
-    <input type="text" name="itsid" value="">
-	<br><br>
-    <input type="submit" value="Submit">
-  </fieldset>
-</form>
+  <div class="header">
+    <h3>Muntasebaat Hifz Registration</h3>
+  </div>
+
+  <form class="container-fluid" method="post">
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="itsid">Its Id</label>
+          <input type="number" class="form-control" name="itsid" >
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="form-group">
+          <label for="fullname">Name</label>
+          <input type="text" class="form-control" name="fullname" >
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" name="email" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="skype">Skype Id</label>
+          <input type="text" class="form-control" name="skype" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="mobile">Mobile No.</label>
+          <input type="text" class="form-control" name="mobile" >
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="email">Place of Residence</label>
+          <input type="text" class="form-control" name="residence" >
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="email">Jamea Farig Year</label>
+          <input type="text" class="form-control" name="farigyear" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="skype">Khidmat (if any?)</label>
+          <input type="text" class="form-control" name="khidmat" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="mobile">Hifz Done Till</label>
+          <input type="text" class="form-control" name="hifzdonetill" >
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="itsid">Hifz Class Time</label>
+          <input type="text" class="form-control" name="hifztime" >
+        </div>
+      </div>
+      <div class="col-md-6 col-sm-12">
+        <div class="form-group">
+          <label for="fullname">Hifz Days</label>
+          <input type="text" class="form-control" name="hifzdays" >
+        </div>
+      </div>
+
+    </div>
+
+    <input class="btn btn-success" type="submit" value="Submit">
+  </form>
 
 </body>
 </html>
