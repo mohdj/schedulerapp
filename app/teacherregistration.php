a4f83d073c044c6d519c1983078e2beea126a9d0
Mufaddal Jamal <mufaddal.jamal@flipkart.com>
2016-10-18 14:33:53 +0530

    syling for teacher-registration page

--- app/teacherregistration.php
+++ app/teacherregistration.php
@@ -16,8 +16,8 @@ if($_POST)
   extract($data);
     $sql = "INSERT INTO teachers(
                                         `ITS`,
-                                        `password`,
                                         `Full_Name`,
+                                        `password`,
                                         `Time`,
                                         `Residence`,
                                         `Hifzyear`,
@@ -27,8 +27,8 @@ if($_POST)
                                         )
                             VALUES (
                                     '$itsid',
-                                    '$password',
                                     '$fullname',
+                                    '$password',
                                     '$hifztime',
                                     '$residence',
                                     '$hafizyear',
@@ -47,41 +47,105 @@ if($_POST)
 
 <!DOCTYPE html>
 <html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+  <title>Hifz Program</title>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <style>
+    .header {
+      min-height: 50px;
+      background: #FFEAC3;
+      text-transform: uppercase;
+      text-align: center;
+      border-bottom: 2px solid #704d0e;
+      margin-bottom: 20px
+    }
+    .header h3 {
+      margin: auto;
+      line-height: 60px;
+    }
+  </style>
+</head>
 <body>
 
-<form method="post">
-  <fieldset>
-    <legend>Muntasebaat Huffaz Registration</legend>
-    Full name:<br>
-    <input type="text" name="fullname" value="">
-    <br>
-    Hifz Khidmat Time:<br>
-    <input type="text" name="hifztime" value="">
-    </br>
-	ITS ID:<br>
-    <input type="text" name="itsid" value="">
-    <br>
-  Password:<br>
-    <input type="password" name="password" value="">
-    <br>
-	Place of Residence:<br>
-    <input type="text" name="residence" value="">
-    <br>
-	Hafiz Year:<br>
-    <input type="text" name="hafizyear" value="">
-    <br>
-	Jamea Farig Year:<br>
-    <input type="text" name="farigyear" value="">
-    <br>
-	Mobile No:<br>
-    <input type="text" name="mobile" value="">
-    <br>
-	Email ID:<br>
-    <input type="email" name="email" value="">
-	<br><br>
-    <input type="submit" value="Submit">
-  </fieldset>
-</form>
+  <div class="header">
+    <h3>Muntasebaat Huffaz Registration</h3>
+  </div>
+
+  <form class="container-fluid" method="post">
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="itsid">Its Id</label>
+          <input type="number" class="form-control" name="itsid" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="itsid">Password</label>
+          <input type="number" class="form-control" name="password" >
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="form-group">
+          <label for="fullname">Name</label>
+          <input type="text" class="form-control" name="fullname" >
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="mobile">Mobile No.</label>
+          <input type="text" class="form-control" name="mobile" >
+        </div>
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <div class="form-group">
+          <label for="email">Email address</label>
+          <input type="email" class="form-control" name="email" >
+        </div>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12 col-sm-12">
+        <div class="form-group">
+          <label for="email">Place of Residence</label>
+          <input type="text" class="form-control" name="residence" >
+        </div>
+      </div>
+
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="email">Jamea Farig Year</label>
+          <input type="text" class="form-control" name="farigyear" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="skype">Hafiz Year</label>
+          <input type="text" class="form-control" name="hafizyear" >
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="form-group">
+          <label for="mobile">Hifz Khidmat Time</label>
+          <input type="text" class="form-control" name="hifztime" >
+        </div>
+      </div>
+    </div>
+
+
+    <input class="btn btn-success" type="submit" value="Submit">
+  </form>
 
 </body>
 </html>
