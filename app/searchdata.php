c23b6aa1f1ba4e0b1ec4c3bad8b0570ef23c0bb1
Mufaddal Jamal <mufaddal.jamal@flipkart.com>
2016-10-22 11:15:32 +0530

    Changes to styling of search-data

--- app/searchdata.php
+++ app/searchdata.php
@@ -40,108 +40,62 @@ if($_GET)
  
 </head>
 <body>
+  
+
 <div class="container-fluid">
-  <div class="header row">
-    <div class="col-md-8">
-      <h3><a href="/">Muntasebaat Hifz</a></h3>
-    </div>
-    <div class="col-md-2 col-md-offset-2">
-      <a href="logout.php">Logout</a>
-    </div>
+
+  <div class="header row" style="text-align: left;">
+    <div class="col-md-12"><h3><a href="/">Muntasebaat Hifz</a></h3></div>
+    <div class="banner col-md-12"><?php echo $_SESSION['login_teacher']; ?> <?php echo $_SESSION['login_user']; ?> | <a href="logout.php">Logout</a></div>
   </div>
-
-  <div class="modal" id="myModal">
-    <div class="modal-dialog">
-      <div class="modal-content">
-         <div class="modal-header">
-           <h4 class="modal-title">Enter hifz data</h4>
-         </div>
-         <div class="modal-body">
-           <div id="hisabform">
-             <input type="date" name="sf_amount_date" value="<?php echo date("Y-m-d") ?>"/>
-             <input type="number" name="mr_juz1" placeholder="Murajeat Juz1"/> 
-             <input type="number" name="mr_marks1" placeholder="Murajeat Marks1"/>
-             <input type="number" name="mr_juz2" placeholder="Murajeat Juz2"/> 
-             <input type="number" name="mr_marks2" placeholder="Murajeat Marks2"/>
-             <input type="number" name="mr_juz3" placeholder="Murajeat Juz3"/> 
-             <input type="number" name="mr_marks3" placeholder="Murajeat Marks3"/>
-
-             <input type="number" name="jh_from" placeholder="Juzhali From Page"/> 
-             <input type="number" name="jh_to" placeholder="Juzhali To Page"/>
-             <input type="number" name="jh_marks" placeholder="Juzhali Marks"/>
-
-             <input type="text" name="jd_surat" placeholder="Jadeed Surat"/>
-             <input type="number" name="jd_to" placeholder="Jadeed To Ayaat"/>
-             <input type="number" name="jd_marks" placeholder="Jadeed Marks"/>
-             <input type="hidden" name="studentits"/>
-             <input type="hidden" name="teacherits"/>
-           </div>
-         </div>
-      </div>
-
-      <div class="modal-footer">
-        <button type="button" class="btn btn-default" data-dismiss="modal" name="cancel">Close</button>
-        <button type="button" class="btn btn-primary" name="save">Save changes</button>
-      </div>
-
-    </div>
-  </div>
-
   
 <!-- This will be displayed only when teacher is logged In -->
 <?php
-if (!empty($_SESSION['login_teacher'])) {
+  if (!empty($_SESSION['login_teacher'])) {
 ?>
-<div class="panel panel-info">
-  <div class="panel-heading">
-    <h3 class="panel-title">Search Hifz Reports</h3>
-  </div>
+<div>
+  <div class="panel panel-info">
+    <div class="panel-heading">
+      <h3 class="panel-title">Search Hifz Reports</h3>
+    </div>
 
-  <div class="panel-body">
-    <p>
-      Enter Student's ITS ID to view / submit new hifz reports.
-    </p>
-    <form class="form-horizontal">
-      <div class="col-md-8 form-group">
-        <input type="number" class="form-control" name="itsid" placeholder="Student's ITS ID"/>
-      </div>
-      <div class="col-md-2 col-md-offset-2">
-        <input class="btn btn-success btn-block" type="submit" value="Search"/>
-      </div>
-    </form>
+    <div class="panel-body">
+      <p>
+        Enter Student's ITS ID to view / submit new hifz reports.
+      </p>
+      <form class="form-horizontal">
+        <div class="col-md-8 form-group">
+          <input type="number" class="form-control" name="itsid" placeholder="Student's ITS ID"/>
+        </div>
+        <div class="col-md-2 col-md-offset-2">
+          <input class="btn btn-primary btn-block" type="submit" value="Search"/>
+        </div>
+      </form>
+    </div>
   </div>
 </div>
-<?php
-}
-if (!empty($values['Fullname']))
-{
-?>
-<div class="page-header"><h3>Hifz Reports</h3></div>
-<h4><?php echo $values['Fullname']; ?></h4>
+<?php } ?>
 
+
+<?php
+  if (!empty($values['Fullname'])) {
+?>
+<div>
+  <div class="page-header"><h3>Hifz Reports</h3></div>
+  <h4><?php echo $values['Fullname']; ?></h4>
+</div>
+
+<div class="table-responsive">
 <table class="table table-bordered table-hover">
   <thead>
     <tr>
       <th rowspan="2">Date</th>
-      <?php
-       if (!empty($_SESSION['login_teacher']))
-        {
-      ?>
-      <th rowspan="2">Teacher ITS</th>
-      <th rowspan="2">Student ITS</th>
-      <?php } ?>
       <th colspan="2">Murajeat 1</th>
       <th colspan="2">Murajeat 2</th>
       <th colspan="2">Murajeat 3</th>
       <th colspan="3">Jadeed</th>
       <th colspan="2">Tasmee</th>
       <th colspan="3">Juzhali</th>
-      <?php
-       if (!empty($_SESSION['login_teacher']))
-        {
-        ?>
-      <th rowspan="2">Action</th>
-      <?php } ?>
     </tr>
     <tr>
       <th>Juz no</th>
@@ -163,18 +117,10 @@ if (!empty($values['Fullname']))
 
   <tbody>
     <?php
-      while($values = mysqli_fetch_assoc($result))
-      {
+      while($values = mysqli_fetch_assoc($result)) {
     ?>
     <tr>
       <td><?php echo $values['date']; ?></td>
-      <?php
-       if (!empty($_SESSION['login_teacher']))
-        {
-        ?>
-      <td><?php echo $values['its_teacher']; ?></td>
-      <td><?php echo $values['its_student']; ?></td>
-      <?php } ?>
       <td><?php echo $values['murajeat_juz1']; ?></td>
       <td><?php echo $values['murajeat_marks1']; ?></td>
       <td><?php echo $values['murajeat_juz2']; ?></td>
@@ -189,25 +135,161 @@ if (!empty($values['Fullname']))
       <td><?php echo $values['juzhali_from']; ?></td>
       <td><?php echo $values['juzhali_to']; ?></td>
       <td><?php echo $values['juzhali_marks']; ?></td>
-      <?php
-       if (!empty($_SESSION['login_teacher']))
-        {
-        ?>
-      <td></td>
-      <?php } ?>
-    </tr>
-    <?php
-       } 
-       if (!empty($_SESSION['login_teacher']))
-        {
-        ?>
-    <tr>
-        <td colspan='17'></td>
-        <td><a href="#" data-key="payhisab" data-teacher-its="<?php echo $_SESSION['login_teacher']; ?>" data-its="<?php echo $student_itsid; ?>"><img src="images/add.png" style="width:20px;height:20px;"></a></td>
     </tr>
     <?php } ?>
   </tbody>
 </table>
+</div>
+
+  <?php
+     if (!empty($_SESSION['login_teacher'])) {
+  ?>
+
+  <div class="page-header"><h3>Add New Report</h3></div>
+
+  <form class="form ro">
+    <input type="hidden" name="studentits" value="12345677">
+    <input type="hidden" name="teacherits" value="12345678">
+
+    <div class="form-group">
+      <label>Date</label>
+      <input class="form-contro" type="date" name="sf_amount_date" value="2016-10-21">
+    </div>
+
+    <div class="row">
+    <div class="entry-box col-md-6">
+      <div class="entry-box-body">
+        <p>
+          <strong>Murajaat 1 Details:</strong>
+        </p>
+        <div class="row">
+           <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Juz No</label>
+              <input class="form-control" type="number" name="mr_juz1" placeholder="1 - 30">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Marks</label>
+              <input class="form-control" type="number" name="mr_marks1" placeholder="? out of 10">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="entry-box col-md-6">
+      <div class="entry-box-body">
+        <p>
+          <strong>Murajaat 2 Details:</strong>
+        </p>
+        <div class="row">
+           <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Juz No</label>
+              <input class="form-control" type="number" name="mr_juz2" placeholder="1 - 30">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Marks</label>
+              <input class="form-control" type="number" name="mr_marks2" placeholder="? out of 10">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="entry-box col-md-6">
+      <div class="entry-box-body">
+        <p>
+          <strong>Murajaat 3 Details:</strong>
+        </p>
+        <div class="row">
+           <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Juz No</label>
+              <input class="form-control" type="number" name="mr_juz3" placeholder="1 - 30">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Marks</label>
+              <input class="form-control" type="number" name="mr_marks3" placeholder="? out of 10">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="entry-box col-md-6">
+      <div class="entry-box-body">
+        <p>
+          <strong>Juz Hali Details:</strong>
+        </p>
+        <div class="row">
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>From Page No</label>
+              <input class="form-control" type="number" name="jh_from" placeholder="1 - 20">
+            </div>
+          </div>
+           <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>To Page No</label>
+              <input class="form-control" type="number" name="jh_to" placeholder="1 to 20">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Marks</label>
+              <input class="form-control" type="number" name="jh_marks" placeholder="? out of 10">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="entry-box col-md-6">
+      <div class="entry-box-body">
+        <p>
+          <strong>Jadeed Details:</strong>
+        </p>
+        <div class="row">
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Surat</label>
+              <input class="form-control" type="text" name="jd_surat" placeholder="surat name">
+            </div>
+          </div>
+           <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>To Ayat No</label>
+              <input class="form-control" type="number" name="jd_to" placeholder="">
+            </div>
+          </div>
+          <div class="col-md-4 col-sm-12">
+            <div class="form-group">
+              <label>Marks</label>
+              <input class="form-control" type="number" name="jd_marks" placeholder="? out of 10">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="entry-box col-md-6">.</div>
+
+    <div class="col-md-12">
+      <button type="button" class="btn btn-success" name="save">Save changes</button>
+    </div>
+  </div>
+  </form>
+  <?php } ?>
+
 <?php } ?>
 <script src="../js/jquery.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
@@ -259,9 +341,10 @@ $(function(){
       });
     });
 </script>
-  <div class="footer">
+  <div class="footer row">
    <span>Copyright &copy; 2015-2016 Muntasebaat Hifz. All rights reserved.</span>
   </div>
 </div>
+
 </body>
 </html>
