0c540e175529fb5e5eff1e7466c2a17e4caa8fb1
j4m4l <mufaddal.jamal@gmail.com>
2016-10-25 14:47:48 +0530

    added few reports

--- app/report.php
+++ app/report.php
@@ -15,18 +15,21 @@ if (mysqli_connect_errno()) {
 if ($_POST) {
   $report_name = $_POST['reportname'];
 
-  switch ($reportname) {
+  switch ($report_name) {
       case "all_student":
-          $query = "SELECT * FROM students";
+          $query = "SELECT `ITS`, `Fullname`, `Farigyear`, `HifzSipara`, `hifzsanad`, `Residence`, `Khidmat`, `Email`, `Skype`, `Mobile`, `Classtime`, `Hifzdays` FROM `students`;";
           break;
       case "all_teacher":
-          $query = "SELECT * FROM teachers";
+          $query = "SELECT `ITS`, `Full_Name`, `Time`, `Residence`, `Hifzyear`, `Farigyear`, `Mobile`, `Whatsapp`, `Email` FROM `teachers`;";
           break;
-      case "test_report":
-          $query = "SELECT * FROM daily_hifz_report limit 5";
+      case "today_hifz_report":
+          $query = "SELECT  `students`.`fullname` as `Student_Name`,  `teachers`.`full_name` as `Teacher_Name` , `daily_hifz_report`.`date`, `daily_hifz_report`.`attendance`, `daily_hifz_report`.`murajeat_juz1`, `daily_hifz_report`.`murajeat_marks1`, `daily_hifz_report`.`murajeat_juz2`, `daily_hifz_report`.`murajeat_marks2`, `daily_hifz_report`.`murajeat_juz3`, `daily_hifz_report`.`murajeat_marks3`, `daily_hifz_report`.`jadeed_surat`, `daily_hifz_report`.`jadeed_to_ayat`, `daily_hifz_report`.`jadeed_marks`, `daily_hifz_report`.`juzhali_from`, `daily_hifz_report`.`juzhali_to`, `daily_hifz_report`.`juzhali_marks`, `daily_hifz_report`.`tasmee`, `daily_hifz_report`.`tasmee_marks` FROM  `daily_hifz_report` JOIN  `students` ON  `daily_hifz_report`.`its_student` =  `students`.`its` JOIN  `teachers` ON  `daily_hifz_report`.`its_teacher` =  `teachers`.`its` WHERE  `daily_hifz_report`.`date` = DATE(NOW());";
+          break;
+      case "yesterday_hifz_report":
+          $query = "SELECT  `students`.`fullname` as `Student_Name`,  `teachers`.`full_name` as `Teacher_Name` , `daily_hifz_report`.`date`, `daily_hifz_report`.`attendance`, `daily_hifz_report`.`murajeat_juz1`, `daily_hifz_report`.`murajeat_marks1`, `daily_hifz_report`.`murajeat_juz2`, `daily_hifz_report`.`murajeat_marks2`, `daily_hifz_report`.`murajeat_juz3`, `daily_hifz_report`.`murajeat_marks3`, `daily_hifz_report`.`jadeed_surat`, `daily_hifz_report`.`jadeed_to_ayat`, `daily_hifz_report`.`jadeed_marks`, `daily_hifz_report`.`juzhali_from`, `daily_hifz_report`.`juzhali_to`, `daily_hifz_report`.`juzhali_marks`, `daily_hifz_report`.`tasmee`, `daily_hifz_report`.`tasmee_marks` FROM  `daily_hifz_report` JOIN  `students` ON  `daily_hifz_report`.`its_student` =  `students`.`its` JOIN  `teachers` ON  `daily_hifz_report`.`its_teacher` =  `teachers`.`its` WHERE  `daily_hifz_report`.`date` = SUBDATE(DATE(NOW()), 1) ;";
           break;
       default:
-          $query = "SELECT 'no reportname provided' as `Error`";
+          $query = "SELECT 'No report name provided.' as `Error`";
           break;
   }
   
@@ -55,61 +58,65 @@ if ($_POST) {
       <h3><a href="/">Admin Reports</a></h3>
     </div>
 
-    <div class="panel panel-info">
-      <div class="panel-heading">
-        <h3 class="panel-title">Search Reports</h3>
+   <div class="container-fluid">
+      <div class="panel panel-info">
+        <div class="panel-heading">
+          <h3 class="panel-title">Search Reports</h3>
+        </div>
+
+        <div class="panel-body">
+          <p>
+            Enter Student's ITS ID to view / submit new hifz reports.
+          </p>
+          <form class="form-horizontal" method="post">
+            <div class="col-md-8 form-group">
+              <select class="form-control" name="reportname">
+                <option value="test_report">Test Report</option>
+                <option value="all_student">All Student Data</option>
+                <option value="all_teacher">All Teacher Data</option>
+              </select>
+            </div>
+            <div class="col-md-2 col-md-offset-2">
+              <input class="btn btn-success btn-block" type="submit" value="Fetch"/>
+            </div>
+          </form>
+        </div>
       </div>
 
-      <div class="panel-body">
-        <p>
-          Enter Student's ITS ID to view / submit new hifz reports.
-        </p>
-        <form class="form-horizontal" method="post">
-          <div class="col-md-8 form-group">
-            <select class="form-control" name="reportname">
-              <option value="test_report">Test Report</option>
-              <option value="all_student">All Student Data</option>
-              <option value="all_teacher">All Teacher Data</option>
-            </select>
-          </div>
-          <div class="col-md-2 col-md-offset-2">
-            <input class="btn btn-success btn-block" type="submit" value="Fetch"/>
-          </div>
-        </form>
-      </div>
-    </div>
+     <div class="table-responsive">
+       <table class="table table-bordered table-hover">
+         <thead>
+           <tr>
+             <?php
+               //showing fields as header, and keeping them in all_propert array
+               while ($property = mysqli_fetch_field($result)) {
+                 array_push($all_property, $property->name);
+             ?>
+               <td><?php echo $property->name; ?></td>
+             <?php } ?>
+           </tr>
+         </thead>
 
-    <div class="table-responsive">
-      <table class="table table-bordered table-hover">
-        <thead>
-          <tr>
+         <tbody>
+           <?php
+             //showing all data
+             while ($row = mysqli_fetch_array($result)) {
+                 echo "<tr>";
+                 foreach ($all_property as $item) {
+                     echo '<td>' . $row[$item] . '</td>'; //get items using property value
+                 }
+                 echo '</tr>';
+             }
+             echo "</table>";  
+           ?>
+         </tbody>
+       </table>
+     </div>
+    
+   </div>
 
-            <?php
-              //showing fields as header, and keeping them in all_propert array
-              while ($property = mysqli_fetch_field($result)) {
-                array_push($all_property, $property->name);
-            ?>
-              <td><?php echo $property->name; ?></td>
-            <?php } ?>
-          </tr>
-        </thead>
-
-        <tbody>
-
-          <?php
-            //showing all data
-            while ($row = mysqli_fetch_array($result)) {
-                echo "<tr>";
-                foreach ($all_property as $item) {
-                    echo '<td>' . $row[$item] . '</td>'; //get items using property value
-                }
-                echo '</tr>';
-            }
-            echo "</table>";  
-          ?>
-        </tbody>
-      </table>
-    </div>
-
+   <div class="footer">
+    <span>Copyright &copy; 2015-2016 Muntasebaat Hifz. All rights reserved.</span>
+   </div>
   </body>
 </html>
