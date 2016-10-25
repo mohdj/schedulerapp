3c883289e80576be06bcfd4034a430d5f242e513
j4m4l <mufaddal.jamal@gmail.com>
2016-10-25 13:13:05 +0530

    updated repot.php

--- app/report.php
+++ app/report.php
@@ -12,19 +12,20 @@ else {
 if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-
 if ($_POST) {
-
   $report_name = $_POST['reportname'];
 
-  $query = "SELECT * FROM daily_hifz_report limit 5";
+  if ($reportname == 'all_student') {
+    $query = "SELECT * FROM students";
+  } else if ($reportname == 'all_teacher') {
+    $query = "SELECT * FROM teachers";
+  } else if ($reportname == 'test_report') {
+    $query = "SELECT * FROM daily_hifz_report limit 5";
+  }
+  
   $result = mysqli_query($link, $query);
-
   $all_property = array();  //declare an array for saving property
-
-  echo $report_name;
 }
-
 ?>
 
 
@@ -47,60 +48,61 @@ if ($_POST) {
       <h3><a href="/">Admin Reports</a></h3>
     </div>
 
+    <div class="panel panel-info">
+      <div class="panel-heading">
+        <h3 class="panel-title">Search Reports</h3>
+      </div>
 
-    <form class="form">
+      <div class="panel-body">
+        <p>
+          Enter Student's ITS ID to view / submit new hifz reports.
+        </p>
+        <form class="form-horizontal" method="post">
+          <div class="col-md-8 form-group">
+            <select class="form-control" name="reportname">
+              <option value="test_report">Test Report</option>
+              <option value="all_student">All Student Data</option>
+              <option value="all_teacher">All Teacher Data</option>
+            </select>
+          </div>
+          <div class="col-md-2 col-md-offset-2">
+            <input class="btn btn-success btn-block" type="submit" value="Fetch"/>
+          </div>
+        </form>
+      </div>
+    </div>
 
-      <select class="form-control" name="reportname">
-        <option value="test_report">Test Report</option>
-        <option value="all_student">All Student Data</option>
-        <option value="all_teacher">All Teacher Data</option>
-      </select>
+    <div class="table-responsive">
+      <table class="table table-bordered table-hover">
+        <thead>
+          <tr>
 
-      <input class="btn btn-success" type="submit" value="Fetch Report"/>
-    </form>
+            <?php
+              //showing fields as header, and keeping them in all_propert array
+              while ($property = mysqli_fetch_field($result)) {
+                array_push($all_property, $property->name);
+            ?>
+              <td><?php echo $property->name; ?></td>
+            <?php } ?>
+          </tr>
+        </thead>
 
-    
-    <table>
-      <thead>
-        <tr>
+        <tbody>
 
           <?php
-            //showing fields as header, and keeping them in all_propert array
-            while ($property = mysqli_fetch_field($result)) {
-              array_push($all_property, $property->name);
+            //showing all data
+            while ($row = mysqli_fetch_array($result)) {
+                echo "<tr>";
+                foreach ($all_property as $item) {
+                    echo '<td>' . $row[$item] . '</td>'; //get items using property value
+                }
+                echo '</tr>';
+            }
+            echo "</table>";  
           ?>
-            <td><?php echo $property->name; ?></td>
-          <?php } ?>
-        </tr>
-      </thead>
-
-      <tbody>
-
-        <?php
-          //showing all data
-          while ($row = mysqli_fetch_array($result)) {
-              echo "<tr>";
-              foreach ($all_property as $item) {
-                  echo '<td>' . $row[$item] . '</td>'; //get items using property value
-              }
-              echo '</tr>';
-          }
-          echo "</table>";  
-        ?>
-      </tbody>
-    </table>
+        </tbody>
+      </table>
+    </div>
 
   </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
